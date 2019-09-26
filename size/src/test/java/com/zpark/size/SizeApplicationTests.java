a6f95b501a4b30cfc55001e4f7bdc16995c28e11
李金超 <943673603@qq.com>
2019-09-26 16:56:20 +0800

    整体框架的三个工程 1.api 2.console 3.site 其中core是父类

---
+++ size/src/test/java/com/zpark/size/SizeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zpark.size;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SizeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
